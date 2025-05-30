53a84c959d9818b7647a103be131f77487e93c13
noufanelachola <noufansadiqelachola@gmail.com>
2025-05-30 14:32:19 +0530

    numpy

---
+++ 01-numpy.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a = np.array([[5, 4, 3], [1, 2, 6]])
+
+# # print(a[0][0])
+# # print(a.ndim)
+# # print(a.shape)
+# # print(a.size)
+
+
+# # Creating a basic array
+# print(np.zeros(2))
+# print(np.ones(2))
+# print(np.empty(5))
+# print(np.linspace(0, 10, num=5))
+
+# b = np.arange(6)
+# print(b)
+# c = b.reshape(2, 3)
+# print(c)
+
+d = np.array([[1, 2, 3]])
+print(d)
