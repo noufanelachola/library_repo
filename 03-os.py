aaf03ae245a26686bed01743f84ae04e7e838d12
noufanelachola <noufansadiqelachola@gmail.com>
2025-05-30 15:16:43 +0530

    os

---
+++ 03-os.py
@@ -0,0 +1,9 @@
+import os
+
+# print(os.getcwd())
+os.chdir('D:/Noufan Elachola/CODES')
+
+# Now print the current working directory
+print("Current working directory:", os.getcwd())
+
+print(os.listdir("."))
